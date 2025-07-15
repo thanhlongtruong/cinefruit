dfc6ab2e1d5050df17494df8440310fcf895f7c0
Thành Long Trương <truongthanhlong1542004@gmail.com>
2025-07-15 13:54:31 +0700

    demo part4

--- lib/config/const.dart
+++ lib/config/const.dart
@@ -35,7 +35,7 @@ final dioProvider = Provider<Dio>((ref) {
 
   final dio = Dio(
     BaseOptions(
-      baseUrl: baseUrl,
+      baseUrl: backupUrl,
       connectTimeout: Duration(seconds: 60),
       receiveTimeout: Duration(seconds: 60),
     ),
